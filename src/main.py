d3e72924b1dfaa6b7e86c48ae9ed9c9ffb697152
Yakari68 <yakariyoutube@laposte.net>
2026-05-07 11:07:40 +0200

    main.py now launches, todo: add futures matches loading, or load them later (not set for now), add qt signals to match.py

--- src/main.py
+++ src/main.py
@@ -18,15 +18,24 @@ if __name__=="__main__":
             teamlist=[Team.new("Skib"),Team.new("None"),Team.new("Hava"),Team.new("Nagila"),Team.new("Verdamm")]
             shuffle(teamlist)
             matchs_init=[MatchWidget(parent=self,top_team=teamlist[i],bottom_team=teamlist[i+1]) for i in range(0,len(teamlist)-1,2)]
-            mainWidget=QWidget(self)
-            layout=QVBoxLayout(mainWidget)
-            rounds=[QWidget for i in range(len(teamlist)+1)]
-            rounds_layouts=[QHBoxLayout(rounds[r]) for r in rounds]
-            for r in rounds:
+            
+             
+            main_widget=QWidget(self)
+            layout=QVBoxLayout(main_widget)           
+            
+            rounds=[QWidget(main_widget) for i in range(len(teamlist)+1)]
+            rounds_layouts=[QHBoxLayout(rounds[i]) for i in range(len(rounds))]
+            for i in range(len(rounds)):
                 for m in matchs_init:
-                    rounds_layouts.addWidget(m)
-            mainWidget.setLayout(layout)
-            self.setCentralWidget(mainWidget)
+                    rounds_layouts[i].addWidget(m)
+            
+            for i in range(len(rounds)):
+                rounds[i].setLayout(rounds_layouts[i])
+
+            for r in rounds:
+                layout.addWidget(r)
+            main_widget.setLayout(layout)
+            self.setCentralWidget(main_widget)
     # Create the Qt Application
     app = QApplication(sys.argv)
 
