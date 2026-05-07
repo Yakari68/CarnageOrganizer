4cd2856398ebab0f72b635cf9fa0165e23f4480c
Yakari68 <yakariyoutube@laposte.net>
2026-05-07 15:08:11 +0200

    updated main.py: can now display power of 2 number of teams. Todo: SIGNALS.

--- src/main.py
+++ src/main.py
@@ -23,27 +23,36 @@ if __name__=="__main__":
 
             # Définition du titre de la fenêtre
             self.setWindowTitle("Hello!")
-            teamlist=[Team.new("Skib"),Team.new("None"),Team.new("Hava"),Team.new("Nagila")] # ,Team.new("Verdamm")]
+#             teamlist=[Team.new("Skib"),Team.new("None"),Team.new("Hava"),Team.new("Nagila")] # ,Team.new("Verdamm")]
+            teamlist=[Team.new("Skib"),Team.new("None"),Team.new("Hava"),Team.new("Nagila") ,Team.new("Verdamm"),Team.new("Koftomi"),Team.new("Arsch"),Team.new("Matcha")]
             shuffle(teamlist)
-            matchs_init=[MatchWidget(parent=self,top_team=teamlist[i],bottom_team=teamlist[i+1]) for i in range(0,len(teamlist)-1,2)] # à généraliser à tout les rounds
             
-             
+            
+            matchs=[[MatchWidget(parent=self,top_team=teamlist[i],bottom_team=teamlist[i+1]) for i in range(0,len(teamlist),2)]]
+            
+            current=1
+            while len(matchs[current-1])!=1:
+                current+=1
+                matchs_to_add=[MatchWidget(parent=self) for k in range(len(matchs[current-2])//2)]
+                matchs.append(matchs_to_add)
+            print(matchs)
+            
             main_widget=QWidget(self)
-            layout=QVBoxLayout(main_widget)           
-            
-            rounds=[QWidget(main_widget) for i in range(len(teamlist)//2)]
+            layout=QVBoxLayout(main_widget)
+
+            rounds=[QWidget(main_widget) for i in range(len(bin(len(teamlist)))-3)]
             rounds_layouts=[QHBoxLayout(rounds[i]) for i in range(len(rounds))]
-            for m in matchs_init:
-                rounds_layouts[0].addWidget(m)
-            next_row_to_add=len(matchs_init)//2
-            for i in range(1,len(rounds)):
-                for m in range(next_row_to_add):
-                    rounds_layouts[i].addWidget(MatchWidget(rounds[i]))
-                    # à modifier en fonction de matchs_init une fois généralisé
-                    MATCHPRECEDENT1.winner.connect(MatchWidget(rounds[i]).top_team.get_team)
-                    MATCHPRECEDENT2.winner.connect(MatchWidget(rounds[i]).top_team.get_team)
-                next_row_to_add//=2
             for i in range(len(rounds)):
+                for m in matchs[i]:
+                    rounds_layouts[i].addWidget(m)
+            for i in range(len(matchs)):
+                for m in matchs[i]:
+                    rounds_layouts[i].addWidget(m)
+                    # à modifier en fonction de matchs_init une fois généralisé
+                    #MATCHPRECEDENT1.winner.connect(MatchWidget(rounds[i]).top_team.get_team)
+                    #MATCHPRECEDENT2.winner.connect(MatchWidget(rounds[i]).top_team.get_team)
+            for i in range(len(rounds)):
+                print(i)
                 rounds[i].setLayout(rounds_layouts[i])
 
             for r in rounds:
