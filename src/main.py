aff4bd16f3e876cb827d4b8b13c83f2a47c276f1
Yakari68 <yakariyoutube@laposte.net>
2026-05-09 08:22:29 +0200

    separated main.py content with a new file: app/ui/main_widget.py. bug found: if pressed enter, text can't be updated for later matchs: if team 1 has already validated its scores, you can't update it anymore. Validate at end of match for now, with lower score first, winner then. TODO: db, bug fix.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QIntValidator
 from app.logics.teams import Team
 from app.ui.match import MatchWidget
+from app.ui.main_widget import mainWidget
 from random import shuffle
 from math import log2
 
@@ -15,42 +16,8 @@ if __name__=="__main__":
 
             # Définition du titre de la fenêtre
             self.setWindowTitle("Hello!")
-#             teamlist=[Team.new("Skib"),Team.new("None"),Team.new("Hava"),Team.new("Nagila")] # ,Team.new("Verdamm")]
-            teamlist=[Team.new("Skib"),Team.new("None"),Team.new("Hava"),Team.new("Nagila") ,Team.new("Verdamm"),Team.new("Koftomi"),Team.new("Arsch"),Team.new("Matcha")]
-            shuffle(teamlist)
-            
-            
-            matchs=[[MatchWidget(parent=self,top_team=teamlist[i],bottom_team=teamlist[i+1]) for i in range(0,len(teamlist),2)]]
-            
-            current=1
-            while len(matchs[current-1])!=1:
-                current+=1
-                matchs_to_add=[MatchWidget(parent=self) for k in range(len(matchs[current-2])//2)]
-                matchs.append(matchs_to_add)
-            
-            main_widget=QWidget(self)
-            layout=QVBoxLayout(main_widget)
-
-            rounds = [QWidget(main_widget) for _ in range(int(log2(len(teamlist))))]
-            rounds_layouts=[QHBoxLayout(rounds[i]) for i in range(len(rounds))]
-            for i in range(len(rounds)):
-                for m in matchs[i]:
-                    rounds_layouts[i].addWidget(m)
-            for i in range(1,len(matchs)):
-                prev_matchs_counter=0
-                for m in matchs[i]:
-                    rounds_layouts[i].addWidget(m)
-                    # à modifier en fonction de matchs_init une fois généralisé
-                    matchs[i-1][prev_matchs_counter].winner.connect(m.set_top_team)
-                    matchs[i-1][prev_matchs_counter+1].winner.connect(m.set_bottom_team)
-                    prev_matchs_counter+=2
-            for i in range(len(rounds)):
-                rounds[i].setLayout(rounds_layouts[i])
-
-            for r in rounds:
-                layout.addWidget(r)
-            main_widget.setLayout(layout)
-            self.setCentralWidget(main_widget)
+            self.main=mainWidget(self)
+            self.setCentralWidget(self.main)
     # Create the Qt Application
     app = QApplication(sys.argv)
 
